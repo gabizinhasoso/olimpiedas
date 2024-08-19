0ccf69a274a452adeaf3626481e9eda48f94eee4
gabizinhasoso <137403513+gabizinhasoso@users.noreply.github.com>
2024-08-19 12:01:24 -0300

    olimpiedas

--- script.js
+++ script.js
@@ -10,25 +10,25 @@ const caixaResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-     enunciado: "A inteligência artificial (IA) é um conjunto de tecnologias que permitem aos computadores executar uma variedade de funções avançadas. Quais são as implicações da IA na privacidade dos dados?",
-       alternativas: [
-           "A IA, embora possa desempenhar um papel fundamental na detecção e prevenção de ameaças à segurança.",
-           "Também pode ser usada como uma ferramenta para a invasão de privacidade."
+     enunciado: "Qual é o mascote oficial dos Jogos Olímpicos de Paris 2024?",
+       alternativa: [
+           " O mascote de Paris 2024 é a Phryge Olímpica. A mascote se baseia nos pequenos gorros tradicionais da Frígia, nos quasi seu formato foi inspirado..",
+           "O nome e o design foram escolhidos como símbolo de liberdade e para representar figuras alegóricas da republica francesa."
        ]
     },
     
     {
-     enunciado: "Inteligência artificial criando artistas 100% virtuais; músicas, obra de arte,incentivando o consumo não consciente  prejudicando a tradição e cultura. Como preservar a tradição e a cultura no atual contexto regido pelas inteligências artificiais",
+     enunciado: "Qual é a diferença entre Jogos Olímpicos de Verão e de Inverno",
       alternativas: [
-           "Realizando pesquisas para saber sobre suas tradições e culturas.",
-            "Alternativa 2"
+           "Os Jogos de Verão, tambem cohecido como Jogos da OLimpíada, é um evento multiesportivo realizado a cada quatro anos, normalmente em julho ou agosto.",
+            "Os Jogos de inverno também saõ realizados a cada quatro anos,no inverno da cidade anfitriã, com competições multiesportivas realizadas na neve e no gelo"
        ]
     },
     {
-        enunciado: "Tomamos como referência que um consumo consciente é aquele do qual o indivíduo compra somente o necessário, Como a sociedade poderia ter um consumo conciente, Buscando informações no IA?",
+        enunciado: "Quais esportes estarão nos Jogos olímpicos de Paris 2024 ",
       alternativas: [
-               "Buscando informação na legislação da saúde",
-               "Confronto das informações da inteligengia artificial e a pessoal"
+               "Basquete 3x3, Tiro com arco, Nado artístico, Boxe, Volei na praia,",
+               "Canoagem de velocidade, saltos ornamentais, Judo,Escalada esportiva"
        ]
     }
      ];
