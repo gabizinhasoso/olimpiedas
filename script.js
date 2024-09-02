065e7c99e52f3850cd656c49e17a9bb64acc1b16
gabizinhasoso <137403513+gabizinhasoso@users.noreply.github.com>
2024-09-02 12:17:00 -0300

    ga

--- script.js
+++ script.js
@@ -1,55 +1,92 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-
 const caixaResultado = document.querySelector(".caixa-resultado");
-
-const caixaResultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-     enunciado: "Qual é o mascote oficial dos Jogos Olímpicos de Paris 2024?",
-       alternativa: [
-           " O mascote de Paris 2024 é a Phryge Olímpica. A mascote se baseia nos pequenos gorros tradicionais da Frígia, nos quasi seu formato foi inspirado..",
-           "O nome e o design foram escolhidos como símbolo de liberdade e para representar figuras alegóricas da republica francesa."
-       ]
-    },
-    
-    {
-     enunciado: "Qual é a diferença entre Jogos Olímpicos de Verão e de Inverno",
-      alternativas: [
-           "Os Jogos de Verão, tambem cohecido como Jogos da OLimpíada, é um evento multiesportivo realizado a cada quatro anos, normalmente em julho ou agosto.",
-            "Os Jogos de inverno também saõ realizados a cada quatro anos,no inverno da cidade anfitriã, com competições multiesportivas realizadas na neve e no gelo"
-       ]
+        enunciado: "A inteligência artificial (IA) é um conjunto de tecnologias que permitem aos computadores executar uma variedade de funções avançadas. Quais são as implicações da IA na privacidade dos dados?",
+        alternativas: [
+            {
+                texto: "A IA, embora possa desempenhar um papel fundamental na detecção e prevenção de ameaças à segurança também acaba divulgando os dados inseridos por outros usuários da plataforma",
+                afirmacao: "Você conhece o futuro tecnológico, porém é bem cauteloso"
+            },
+            {
+                texto: "Também pode ser usada como uma ferramenta para a invasão de privacidade.",
+                afirmacao: "Se sente totalmente inseguro quanto ao uso do IA, pensa em proteger a todo custo suas informações"
+            }
+        ]
     },
     {
-        enunciado: "Quais esportes estarão nos Jogos olímpicos de Paris 2024 ",
-      alternativas: [
-               "Basquete 3x3, Tiro com arco, Nado artístico, Boxe, Volei na praia,",
-               "Canoagem de velocidade, saltos ornamentais, Judo,Escalada esportiva"
-       ]
+        enunciado: "Inteligência artificial criando artistas 100% virtuais; músicas, obra de arte,incentivando o consumo não consciente  prejudicando a tradição e cultura. Como preservar a tradição e a cultura no atual contexto regido pelas inteligências artificiais",
+        alternativas: [
+            {
+                texto: "Realizando pesquisas para saber sobre suas tradições e culturas.",
+                afirmacao: "Para você a IA e ética andam juntas"
+            },
+            {
+                texto: "trabalhando a influência das tradições e culturas em nosso cotidiano",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Tomamos como referência que um consumo consciente é aquele do qual o indivíduo compra somente o necessário, Como a sociedade poderia ter um consumo conciente, Buscando informações no IA?",
+        alternativas: [
+            {
+                texto: "Buscando informação na legislação da saúde",
+                afirmacao: "Prefere a antiga e boa forma de pesquisa, sempre indicando fontes confiáveis "
+            },
+            {
+                texto: "Confronto das informações da inteligengia artificial e a pessoal",
+                afirmacao: "Acredita que tem boas opiniões, mas se deixa levar pelo o que a IA apresenta"
+            }
+        ]
     }
-     ];
+   
+];
 
-     let atual = 0;
-     let perguntaAtual;
 
-function mostraPergunta(){
-     perguntaAtual = perguntas[atual];
-     caixaPerguntas.textContent = perguntaAtual.enunciado;
-     caixaAlternativas.textContent= "";
-     mostraAlternativas();
-    
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
     }
-function mostraAlternativas(){
-   for(const alternativa of perguntaAtual.alternativas){
-    const botaoAlternativas= document.creatElement("button");
-    botaoAlternativas.textContent = alternativa;
-    caixaAlternativas.appendChild(botaoAlternativas);
-
-   }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
-     mostraPergunta();
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacao +" ";
+    atual++;
+    mostraPergunta();
+}
+
+
+
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "IA mostra perfil de quem ajuda o meio ambiente!";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
+    
